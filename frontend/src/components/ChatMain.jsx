56d087559a42f6c42297a992200011129fd110af
sirousagisan <katopillow@gmail.com>
2024-02-22 22:05:04 +0900

    fix

--- frontend/src/components/ChatMain.jsx
+++ frontend/src/components/ChatMain.jsx
@@ -10,36 +10,35 @@ const ChatMain = () => {
   const { handleSubmit, watch, register, reset } = useForm()
   const [inputValue, setInputValue] = useState('');
   const [textareaHeight, setTextareaHeight] = useState('auto');
-  const [messages, setMessages] = useState([]);{}
+  const [messages, setMessages] = useState([]);
   const messagesEndRef = useRef(null); 
+
+  const onSubmit = (data) => {
+    // フォームの送信処理
+    console.log(data.content);
+    setInputValue("")
+    setMessages([...messages, {type: "user", message: inputValue}])
+    reset();
+  };
+
+  const onError = (errors) => {
+    // バリデーションエラー時の処理
+    console.error(errors);
+  };
+
+  const handleKeyDown = (event) => {
+    // Cmd + Enter もしくは Ctrl + Enter
+    if (event.key === "Enter" && (event.ctrlKey || event.metaKey)) {
+      handleSubmit(onSubmit, onError)();
+    }
+  };
 
   useEffect(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
     }
   }, [messages]);
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter" && !e.shiftKey && !e.metaKey && !e.altKey && !e.ctrlKey) {
-      e.preventDefault();
-      if (inputValue.replaceAll('\n', '') === "") {
-        return 
-      }
-    }
-  const submit = () => {
-      setInputValue("")
-      setMessages([...messages, {type: "user", message: inputValue}])
-      setTextareaHeight('auto');
-    }
-  const enterPost = (keyEvent) => {
-      if (!isCompositionend) {
-        return
-      }
-      if (keyEvent.key === 'Enter' && (keyEvent.ctrlKey || keyEvent.metaKey)) {
-        submit()
-        setIsCompositionend(false)
-      }
-    }
-  };
+  
   return (
 <>
   
@@ -47,15 +46,15 @@ const ChatMain = () => {
   <div className="flex flex-col justify-center h-screen lg:mx-60 xl:mx-96 ">
     <div className="flex flex-row min-w-full h-full my-8 overflow-hidden" >
       <div className='flex flex-col'>
+      <MessageCard key={0} message={"こんにちは。質問をどうぞ！"} type={"bot"} />
       {messages.map((msg, idx) => {
         return <MessageCard key={idx} message={msg.message} type={msg.type} />
       })}
-      <MessageCard key={0} message={"aaa"} type={"bot"} />
       </div>
       <div ref={messagesEndRef} />
       <div></div>
     </div>
-    <form className="flex flex-row mb-4" onSubmit={2}>
+    <form className="flex flex-row mb-4" onSubmit={handleSubmit(onSubmit, onError)}>
       <textarea  type="text" className='sticky bottom-2 self-item-center bg-stone-800 text-gray-200 py-2 px-4 border-2 border-gray-400 rounded-xl text-md resize-none overflow-hidden w-full ' 
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
